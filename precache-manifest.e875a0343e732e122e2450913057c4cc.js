6c89768b3b69d35b452d14a295fa06997018bece
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2020-07-26 14:13:45 -0700

    Updates

---
+++ precache-manifest.e875a0343e732e122e2450913057c4cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa8c10ce1a0a6bc886e9b1b9a2c2d94",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4d0f65371141ca873bf6",
+    "url": "/my-app/static/css/2.4e1b8863.chunk.css"
+  },
+  {
+    "revision": "9704a64017a9538e52fb",
+    "url": "/my-app/static/css/main.3e320bc9.chunk.css"
+  },
+  {
+    "revision": "4d0f65371141ca873bf6",
+    "url": "/my-app/static/js/2.1ff7e8cf.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-app/static/js/2.1ff7e8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9704a64017a9538e52fb",
+    "url": "/my-app/static/js/main.4f7262f6.chunk.js"
+  },
+  {
+    "revision": "4dee111c9acc95b7f584",
+    "url": "/my-app/static/js/runtime-main.59a9665e.js"
+  }
+]);
